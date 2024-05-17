06f817a59c2d564eddbd7356aeb33a12dce4d841
Tony <anthonyreagan126@gmail.com>
2024-05-17 13:23:00 +0300

    initial commit

---
+++ client/src/components/PostEdit.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Edit, SimpleForm, TextInput, DateInput} from 'react-admin'
+
+const PostEdit = (props) => {
+  return (
+    <Edit title='Edit  post' {...props}>
+        <SimpleForm>
+            <TextInput disabled source='id' />
+            <TextInput source='title' />
+            <TextInput multiline source='title' />
+            <DateInput label='Publish' source='publishedAt'/>
+        </SimpleForm>
+
+    </Edit>
+  )
+}
+
+export default PostEdit
